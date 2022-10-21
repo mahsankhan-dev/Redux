9ad45335d858562c04065e781d5e152b99b9b625
Ahsan Khan <100356865+mahsankhan-dev@users.noreply.github.com>
2022-10-21 11:26:23 -0700

    Addd function

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -3,14 +3,18 @@ import { useDispatch, useSelector } from "react-redux";
 import { addTodo, deleteTodo } from "../../app/actions";
 
 const Todo = () => {
-  const [input, setInput] = useState();
+  const [input, setInput] = useState("");
   const list = useSelector((state) => state.todoReducers.list);
   const dispatch = useDispatch();
+
+  const addTodoMethods = () => {
+    dispatch(addTodo(input));
+  };
   return (
     <>
       <div>
         <input type="text" onChange={(event) => setInput(event.target.value)} />
-        <button onClick={() => dispatch(addTodo(input))}>Add</button>
+        <button onClick={addTodoMethods}>Add</button>
         <ol>
           {list.map((items) => {
             return (
